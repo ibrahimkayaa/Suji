351225c08166bba8882310b19873345723b879fd
ibrahimkayaa <ibrahimkaya@outlook.com>
2018-12-10 20:20:07 +0300

    New Functions Added

--- Suji.cs
+++ Suji.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
@@ -105,6 +103,82 @@ namespace SJ{
         }
 
         /// <summary>
+        /// Gets the random number with percentage from zero with dictionary.
+        /// </summary>
+        /// <returns>The random number with percentage from zero with dictionary.</returns>
+        /// <param name="dictionary">Dictionary.</param>
+        public static int GetRandomNumberWithPercentageFromZeroWithDictionary(Dictionary<int, int> dictionary)
+        {
+            int retInt = 0;
+            List<int> randomizeList = new List<int>();
+
+            for(int i = 0; i < dictionary.Count; i++)
+            {
+
+                int percentageIndex = dictionary.ElementAt(i).Value;
+
+                int startIndex = 0;
+                while(startIndex < dictionary.ElementAt(i).Value)
+                {
+                    randomizeList.Add(dictionary.ElementAt(i).Key);
+                    startIndex++;
+                }
+            }
+
+            retInt = Random.Range(0,randomizeList.Count);
+            return randomizeList[retInt];
+        }
+
+        /// <summary>
+        /// Sets the percentages list.
+        /// </summary>
+        /// <returns>The percentages list.</returns>
+        /// <param name="dictionary">Dictionary.</param>
+        public static List<int> SetPercentagesList(Dictionary<int,int> dictionary)
+        {
+            List<int> randomizeList = new List<int>();
+
+            for (int i = 0; i < dictionary.Count; i++)
+            {
+
+                int percentageIndex = dictionary.ElementAt(i).Value;
+
+                int startIndex = 0;
+                while (startIndex < dictionary.ElementAt(i).Value)
+                {
+                    randomizeList.Add(dictionary.ElementAt(i).Key);
+                    startIndex++;
+                }
+            }
+
+           
+            return randomizeList;
+        }
+        /// <summary>
+        /// Sets the percentages list.
+        /// </summary>
+        /// <returns>The percentages list.</returns>
+        /// <param name="intValue">İnt value.</param>
+        public static List<int> SetPercentagesList(List<int> intValue){
+            
+            List<int> randomizeList = new List<int>();
+            for (int i = 0; i < intValue.Count;i++){
+
+               
+                int startIndex = 0;
+
+                while(startIndex < intValue[i]){
+
+                    randomizeList.Add(i);
+                    startIndex++;
+                }
+
+            }
+
+            return randomizeList;
+        }
+
+        /// <summary>
         /// Gets the random number with percantage Range(1,percantes.Length).
         /// </summary>
         /// <returns>The random number with percantage from one.</returns>
@@ -167,6 +241,97 @@ namespace SJ{
         }
 
         /// <summary>
+        /// Get Complimentary Color
+        /// </summary>
+        /// <param name="getColor"></param>
+        /// <returns></returns>
+        public static Color GetComplimentaryColor(Color getColor)
+        {
+
+            float h, s, v;
+            Color newColor = getColor;
+            Color.RGBToHSV(getColor, out h, out s, out v);
+
+            //Debug.Log("h : " + h);
+            float newH = h * 360;
+            //Debug.Log("new h : " + newH);
+
+            if(newH > 180)
+            {
+
+                newH -= 180f;
+            }
+            else
+            {
+                newH += 180f;
+            }
+
+            newH = newH / 360f;
+
+            newColor = Color.HSVToRGB(newH, s, v);
+            return newColor;
+        }
+
+        /// <summary>
+        /// Dark the color.
+        /// </summary>
+        /// <returns>Darkest color of the given color with percentage.</returns>
+        /// <param name="getColor">Get color.</param>
+        /// <param name="percantage">Percantage.</param>
+        public static Color DarkColor(Color getColor, int percantage)
+        {
+            float h, s, v;
+            Color newColor = getColor;
+            Color.RGBToHSV(getColor, out h, out s, out v);
+
+
+            if (v < 0.001f)
+            {
+                return newColor;
+            }
+            else
+            {
+
+                float pFloat = (float)percantage / 100f;
+
+                v = (v * (1f - pFloat));
+                newColor = Color.HSVToRGB(h, s, v);
+            }
+
+            return newColor;
+
+
+        }
+
+        /// <summary>
+        /// Gets The Triad Color combination with given color
+        /// </summary>
+        /// <param name="inputColor">İnput color.</param>
+        /// <param name="secondColor">Second color.</param>
+        /// <param name="thirdColor">Third color.</param>
+        public static void GetTriadColor(Color inputColor, out Color secondColor, out Color thirdColor)
+        {
+
+            float h, s, v;
+            secondColor = Color.white;
+            thirdColor = Color.white;
+            Color.RGBToHSV(inputColor, out h, out s, out v);
+
+            float tempH = h * 360f;
+
+            tempH += 120f;
+            if (tempH >= 360) tempH -= 360f;
+            tempH = tempH / 360f;
+            secondColor = Color.HSVToRGB(tempH, s, v);
+            tempH = tempH * 360f;
+            tempH += 120f;
+            if (tempH >= 360) tempH -= 360f;
+            tempH = tempH / 360f;
+            thirdColor = Color.HSVToRGB(tempH, s, v);
+
+        }
+
+        /// <summary>
         /// Saturation of the color with percantage
         /// </summary>
         /// <returns>The  saturated color.</returns>
@@ -198,6 +363,16 @@ namespace SJ{
 
 
             return newColor;
+        }
+
+        //[System.Diagnostics.Conditional("DEVELOPMENT_BUILD")]
+        public static void Log(string message)
+        {
+
+#if UNITY_EDITOR
+            Debug.Log("sj-log : " + message);
+#endif
+
         }
 	}
 
